a3ac767cf9d427a58329adef495e8a0d05e2f4f9
Ben Johnson <benbjohnson@yahoo.com>
2013-05-12 15:01:19 -0600

    Initialization.

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'pg', '~> 0.15.1'
+gem 'jquery-rails', '~> 2.2.1'
+gem 'thin', '~> 1.5.1'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development do
+  gem 'debugger', '~> 1.5.0'
+  gem 'capistrano', '~> 2.15.4'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Landmark::Application.config.secret_token = '5e5fb2b848708aec5b6cd2e3fec8d8d411de4238fe4064ad00c7108d631c604ccf8c0dd234efb8b51c820d348b82fa5c45b77aaaaaf17094f6a2f94c678e0ccc'
