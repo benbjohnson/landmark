d497f3b6603c2fc4d0d07b8c45e95004c203f6fd
Ben Johnson <benbjohnson@yahoo.com>
2013-05-14 21:36:55 -0600

    Began typeahead.js integration.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,9 @@
 $(document).ready(function() {
     $("select[name='large']").selectpicker({style: 'btn-large', menuStyle: 'dropdown-inverse'});
     $('[data-toggle="radio"]').radio();
+
+  $('#flow .search').typeahead([{
+    name: 'search',
+    local: ['View Home Page', 'Signup']
+  }]);
 });
