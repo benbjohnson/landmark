4da1991ea738fef9d8d33767f6c5557c26e2f168
Ben Johnson <benbjohnson@yahoo.com>
2013-06-21 22:16:01 -0600

    Normalize node links.

--- spec/javascripts/flows/show_spec.js
+++ spec/javascripts/flows/show_spec.js
@@ -5,7 +5,7 @@ describe("flows/show", function () {
   //
   //----------------------------------------------------------------------------
 
-  var query =
+  var QUERY =
     new Query({sessionIdleTime:7200, steps:[
       new QueryCondition({expression:"action == '/index.html'", steps:[
         new QuerySelection({name:"0.0", dimensions:["action"], fields:[new QuerySelectionField({name:"count", expression:"count()"})]}),
@@ -15,6 +15,11 @@ describe("flows/show", function () {
       ]})
     ]});
 
+  var NODES = [
+    {id:'0.0-/index.html', name:'/index.html', value:20, depth:0, subdepth:0, selection:QUERY.steps[0].steps[0]},
+    {id:'1.0-/signup.html', name:'/signup.html', value:10, depth:1, subdepth:0, selection:QUERY.steps[0].steps[1].steps[0]},
+    {id:'1.0-/about-us.html', name:'/about-us.html', value:2, depth:1, subdepth:0, selection:QUERY.steps[0].steps[1].steps[0]},
+  ];
 
   //----------------------------------------------------------------------------
   //
@@ -22,8 +27,8 @@ describe("flows/show", function () {
   //
   //----------------------------------------------------------------------------
 
-  it("normalizes returned query data", function() {
-    var nodes = landmark.show.normalize(query, {
+  it("normalizes query results into nodes", function() {
+    var nodes = landmark.show.normalize(QUERY, {
       "0.0": {
         "action": {
           "/index.html": {
@@ -44,8 +49,15 @@ describe("flows/show", function () {
     });
     
     expect(nodes.length).toEqual(3);
-    expect(nodes[0]).toEqual({id:'0.0-/index.html', name:'/index.html', value:20, depth:0, subdepth:0, selection:query.steps[0].steps[0]});
-    expect(nodes[1]).toEqual({id:'1.0-/signup.html', name:'/signup.html', value:10, depth:1, subdepth:0, selection:query.steps[0].steps[1].steps[0]});
-    expect(nodes[2]).toEqual({id:'1.0-/about-us.html', name:'/about-us.html', value:2, depth:1, subdepth:0, selection:query.steps[0].steps[1].steps[0]});
+    expect(nodes[0]).toEqual(NODES[0]);
+    expect(nodes[1]).toEqual(NODES[1]);
+    expect(nodes[2]).toEqual(NODES[2]);
+  });
+
+  it("normalizes nodes into links", function() {
+    var links = landmark.show.links(NODES);
+    expect(links.length).toEqual(2);
+    expect(links[0]).toEqual({source:NODES[0], target:NODES[1], value:10});
+    expect(links[1]).toEqual({source:NODES[0], target:NODES[2], value:2});
   });
 });
