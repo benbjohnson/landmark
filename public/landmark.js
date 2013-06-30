2b5612bb80926e8714fa38ee490a613e0eec6191
Ben Johnson <benbjohnson@yahoo.com>
2013-06-29 23:08:48 -0600

    Upgrade landmark.js (abe796b).

--- public/landmark.js
+++ public/landmark.js
@@ -6,13 +6,12 @@
     //
     //--------------------------------------------------------------------------
 
+    host : "landmark.io",
+    port : 80,
+
     // The user identifier & data.
     userId : null,
     traits : null,
-
-    // The pending event to be sent once the library is initialized.
-    pendingEvent : null,
-    pendingCallback : null,
 
     // A flag stating if user level data has been sent to the server yet.
     // User level data will be sent with the first event or will be sent
@@ -31,13 +30,48 @@
     //----------------------------------
 
     /**
-     * Initializes the Landmark API.
+     * Initializes the Landmark API with the API key.
      *
      * @param {String} apiKey  The API key.
      */
     initialize : function(apiKey) {
       this.apiKey = apiKey;
       return true;
+    },
+
+    /**
+     * @private
+     * Internal initialization for the API once the web page is loaded.
+     */
+    __initialize__ : function() {
+      this.initialized = true;
+
+      // Send off identification if a track() hasn't already sent it.
+      this.send();
+    },
+
+    /**
+     * @private
+     * Internal function used for testing.
+     */
+    __uninitialize__ : function() {
+      this.initialized = false;
+    },
+
+
+    //----------------------------------
+    // Array-style interface
+    //----------------------------------
+
+    /**
+     * Executes a method on the landmark object using the push() method.
+     *
+     * @param {String} methodName  The name of the method to execute.
+     * @param {Array} arguments  A list of arguments to pass to the method.
+     */
+    push : function(methodName) {
+      var args = Array.prototype.slice.call(arguments, 1);
+      return landmark[methodName].apply(landmark, args);
     },
 
 
@@ -71,19 +105,10 @@
      * @param {String} action      The name of the action to track.
      * @param {Object} properties  The action properties.
      */
-    track : function(action, properties, callback) {
+    track : function(action, properties) {
       if(typeof(properties) != "object") properties = {};
-      event = this.extend({}, properties, {action:action});
-      
-      // If the library has been initialized then send it.
-      if(this.initialized) {
-        return this.send(event, callback)
-      }
-      // Otherwise wait for initialization and it'll be sent later.
-      else {
-        this.pendingEvent = event;
-        this.pendingCallback = callback;
-      }
+      event = extend({}, properties, {action:action});
+      return this.send(event)
     },
 
     /**
@@ -91,23 +116,26 @@
      *
      * @param {Object} data  The event data to send.
      */
-    send : function(data, callback) {
+    send : function(data) {
+      // Notify the JavaScript console if the user don't have an API key set.
       if(!this.apiKey) {
-        self.log("[landmark] API Key required. Please call landmark.initialize() first.");
+        this.log("[landmark] API Key required. Please call landmark.initialize() first.");
         return;
       }
-      if(!this.userId) {
-        this.log("[landmark] User identifier required. Please call landmark.identify() first.");
+
+      // Create an event object with just the traits and properties and exit
+      // if there aren't any to send.
+      var event = extend({}, this.traits, data);
+      this.traits = null;
+      if(isEmpty(event)) {
         return;
       }
       
-      var event = this.extend({}, this.traits, data, {apiKey: this.apiKey, id: this.userId});
-      if(event['action']) {
-        event['_action'] = event['action'];
-        delete event['action'];
-      }
+      // Add in the API key and user id.
+      event = extend(event, {apiKey: this.apiKey, id: this.userId});
 
-      return this.post("/track", event, callback);
+      // Send event data to "POST /track".
+      return this.post("/track", event);
     },
 
     
@@ -126,89 +154,112 @@
     },
 
     /**
-     * Sends a JSON object over XHR POST.
+     * Creates a new XHR object, if possible.
      * 
-     * @param {String} url   The url to send to.
+     * @param {String} path   The path to send to.
      * @param {Object} data  The object to convert to JSON and send.
-     * @param {Function} callback  The function to call when successfully executed.
      *
      * @return {XMLHTTPRequest}  The XHR that was created.
      */
-    post : function(url, data, callback) {
+    createXMLHttpRequest : function(method, path, loadHandler, errorHandler) {
+      var url = location.protocol + "//" + this.host + ":" + this.port + path;
       var xhr = new XMLHttpRequest();
       if("withCredentials" in xhr) {
         xhr.open("POST", url, true);
       } else if (typeof XDomainRequest != "undefined") {
         xhr = new XDomainRequest();
-        xhr.open(method, url);
+        xhr.open("POST", url);
       } else {
-        self.log("[landmark] CORS not supported in this browser.");
+        this.log("[landmark] CORS not supported in this browser.");
         return null;
       }
-      var self = this;
       
-      // Response handlers.
-      xhr.onload = function() {
-        var response = {};
-        try { response = JSON.parse(xhr.responseText); } catch(e){}
-        if(typeof(callback) == "function") {
-          callback(response);
-        }
-      };
-      xhr.onerror = function() {
-        var response = {};
-        try { response = JSON.parse(xhr.responseText); } catch(e){}
-        self.log("[landmark] POST " + url + " " + response.message, data);
-      };
+      xhr.onload = loadHandler;
+      xhr.onerror = errorHandler;
 
-      // Send request.
       xhr.setRequestHeader("Content-type", "application/json");
       xhr.setRequestHeader("Cache-Control", "no-cache");
-      xhr.send(JSON.stringify(data));
+      return xhr;
     },
 
     /**
-     * Merges the properties of multiple objects together.
-     * Original: https://github.com/documentcloud/underscore/blob/master/underscore.js
+     * Sends a JSON object over XHR POST.
+     * 
+     * @param {String} path   The path to send to.
+     * @param {Object} data  The object to convert to JSON and send.
      *
-     * @param {Object} obj  The object to extend.
+     * @return {XMLHTTPRequest}  The XHR that was created.
      */
-    extend : function(obj) {
-      if(typeof(obj) != "object") return;
-
-      var args = Array.prototype.slice.call(arguments, 1);
-      for(var i=0; i<args.length; i++) {
-        source = args[i];
-        if (typeof(source) == "object" && source != null && source != undefined) {
-          for (var property in source) {
-            obj[property] = source[property];
-          }
+    post : function(path, data) {
+      var self = this;
+      var xhr = this.createXMLHttpRequest("POST", path,
+        function() {},
+        function() {
+          var response = {};
+          try { response = JSON.parse(xhr.responseText); } catch(e){}
+          self.log("[landmark] POST " + path, response, data);
         }
-      }
-      return obj;
-    },
-
-    /**
-     * Performs a shallow clone of an object.
-     */
-    clone : function(obj) {
-      if(!obj) return;
-      return this.extend({}, obj);
+      );
+      if(xhr == null) return null;
+      
+      // Send request.
+      xhr.send(JSON.stringify(data));
+      return xhr;
     },
   };
+
+  //--------------------------------------------------------------------------
+  //
+  // Private Methods
+  //
+  //--------------------------------------------------------------------------
+  
+  /**
+   * Checks if an object has any keys/values.
+   * Original: https://github.com/documentcloud/underscore/blob/master/underscore.js
+   */
+  function isEmpty(obj) {
+    if(obj == null) return true;
+    for(var key in obj) {
+      if(hasOwnProperty.call(obj, key)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  /**
+   * Merges the properties of multiple objects together.
+   * Original: https://github.com/documentcloud/underscore/blob/master/underscore.js
+   *
+   * @param {Object} obj  The object to extend.
+   */
+  function extend(obj) {
+    if(typeof(obj) != "object") return;
+
+    var args = Array.prototype.slice.call(arguments, 1);
+    for(var i=0; i<args.length; i++) {
+      var source = args[i];
+      if (typeof(source) == "object" && source != null && source != undefined) {
+        for (var property in source) {
+          obj[property] = source[property];
+        }
+      }
+    }
+    return obj;
+  }
+
+  //--------------------------------------------------------------------------
+  //
+  // Events
+  //
+  //--------------------------------------------------------------------------
 
   // Wrap existing onload.
   var onload = window.onload;
   window.onload = function() {
-    landmark.initialized = true;
-
-    // Call existing onload handler.
+    landmark.__initialize__();
     if(typeof(onload) == "function") onload();
-
-    // Delay first action until after initialization.
-    if(landmark.traits || landmark.pendingEvent) {
-      landmark.send(landmark.pendingEvent, landmark.pendingCallback);
-    }
   }
 
   window.landmark = landmark;
