ad4fb2a5a1fc6254de6077a0e8deafb7ee20cd26
Ben Johnson <benbjohnson@yahoo.com>
2013-07-18 06:40:39 -0600

    Add mailcatcher.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,4 +36,8 @@ Landmark::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.default_url_options = { :host => 'landmark.dev' }
+
+  # Mailcatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
