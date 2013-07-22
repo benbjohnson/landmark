e165df446a97ced0327d25ea5fc4ff8fa3537b45
Ben Johnson <benbjohnson@yahoo.com>
2013-07-21 18:25:54 -0600

    Update landmark.js to simplify integration.

--- public/landmark.js
+++ public/landmark.js
@@ -16,6 +16,10 @@
     userId : null,
     traits : null,
 
+    // A list of pending tracking events to be sent after the library is
+    // initialized.
+    pending : [],
+
     // A flag stating if user level data has been sent to the server yet.
     // User level data will be sent with the first event or will be sent
     // by itself if no event is available after initialization.
@@ -47,10 +51,21 @@
      * Internal initialization for the API once the web page is loaded.
      */
     __initialize__ : function() {
+      // Prepend a page view if one has not been made.
+      var pageViewCount = this.pending.filter(function(e) { return e && e.__action__ == "page_view"}).length;
+      if(pageViewCount == 0) {
+        this.trackPageView();
+        this.pending.unshift(this.pending.pop());
+      }
+
       this.initialized = true;
 
-      // Send off identification if a track() hasn't already sent it.
-      this.send();
+      // Send all pending sends.
+      for(var i=0; i<this.pending.length; i++) {
+        this.send(this.pending[i]);
+      }
+
+      this.pending = [];
     },
 
     /**
@@ -61,6 +76,7 @@
       this.initialized = false;
       this.userId = null;
       this.traits = null;
+      this.pending = [];
     },
 
 
@@ -114,7 +130,7 @@
       this.userId = userId;
       this.traits = traits;
 
-      if(this.initialized && this.traits) {
+      if(this.initialized && !isEmpty(this.traits)) {
         this.send();
       }
     },
@@ -129,33 +145,39 @@
      *
      * @param {String} action      The name of the action to track.
      * @param {Object} properties  The action properties.
+     * @param {Object} options
      */
-    track : function(action, properties) {
+    track : function(action, properties, options) {
       if(typeof(properties) != "object") properties = {};
-      var event = extend({}, properties, {action:action});
+      if(typeof(options) != "object") options = {};
+
+      // Create path with optional hash.
+      var path = this.pathname();
+      if(options.includeHash) {
+        path += window.location.hash
+      }
+
+      // Construct the base parameters to track.
+      var base = {
+        __channel__: "web",
+        __action__: action,
+        __uri__: this.normalize(path),
+        __path__: path,
+      };
+
+      // Send event to server.
+      var event = extend(base, properties);
       return this.send(event);
     },
 
     /**
-     * Tracks the current page. By default, the path is normalized to remove
-     * sections that start with numbers.
+     * Tracks the a page view on the current url.
      *
      * @param {Object} properties  The action properties.
+     * @param {Object} options
      */
-    trackPage : function(properties, options) {
-      if(typeof(properties) != "object") properties = {};
-      if(typeof(options) != "object") options = {};
-      if(options.normalize != false) options.normalize = true;
-
-      var action = this.pathname();
-      if(options.includeHash) {
-        action += window.location.hash
-      }
-      if(options.normalize) {
-        action = this.normalize(action);
-      }
-      
-      return this.track(action, properties);
+    trackPageView : function(properties, options) {
+      return this.track('__page_view__', properties, options);
     },
 
     /**
@@ -164,9 +186,22 @@
      * @param {Object} properties  The event properties to send.
      */
     send : function(properties) {
+      // If the library hasn't initialized then put it into a pending queue.
+      if(!this.initialized) {
+        this.pending.push(properties);
+        return;
+      }
+
       // Notify the JavaScript console if the user don't have an API key set.
       if(!this.apiKey) {
         this.log("[landmark] API Key required. Please call landmark.initialize() first.");
+        return;
+      }
+
+      // Don't track if there's no id and cookies are not enabled.
+      var userIdAvailable = (this.userId && (typeof(this.userId) == "number" || typeof(this.userId) == "string"));
+      if(!userIdAvailable && !getCookiesEnabled()) {
+        this.log("[landmark] Cookies not enabled.");
         return;
       }
 
@@ -187,7 +222,7 @@
       var path = "/track";
       path += "?apiKey=" + encodeURIComponent(this.apiKey);
       path += "&t=" + encodeURIComponent(t);
-      if(this.userId && (typeof(this.userId) == "number" || typeof(this.userId) == "string")) {
+      if(userIdAvailable) {
         path += "&id=" + encodeURIComponent(this.userId);
       }
       if(!isEmpty(traits)) {
@@ -213,7 +248,6 @@
       return xhr;
     },
 
-    
 
     //----------------------------------
     // Utility
@@ -373,13 +407,72 @@
   }
 
   /**
+   * Checks to see if cookies are enabled.
+   * 
+   * @return {Boolean}  True if cookies are enabled. Otherwise, false.
+   */
+  function getCookiesEnabled() {
+    if(typeof navigator.cookieEnabled == "undefined") {
+      document.cookie="testcookie";
+      if(document.cookie.indexOf("testcookie") != -1) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return navigator.cookieEnabled;
+    }
+  }
+
+  /**
    * Retrieves the name of the id cookie.
    */
   function cookieId() {
     return "__ldmkid";
   }
 
-  landmark.__test__ = {setCookie: setCookie, getCookie: getCookie};
+  //----------------------------------
+  // Process invocation
+  //----------------------------------
+
+  /**
+   * Processes method calls and arguments stored in an array.
+   *
+   * @param {Array} arr  A list of methods and arguments.
+   */
+  function processInvocations(arr) {
+    if(Object.prototype.toString.call(arr) != "[object Array]") return;
+
+    // Separate out method invocations with their arguments.
+    var invocations = [];
+    var unprocessed = [];
+    for(var i=0; i<arr.length; i++) {
+      if(typeof(arr[i]) == "string" && typeof(landmark[arr[i]]) == "function") {
+        invocations.push({name:arr[i], arguments:[]})
+      } else if(invocations.length > 0) {
+        invocations[invocations.length-1].arguments.push(arr[i]);
+      } else {
+        unprocessed.push(arr[i]);
+      }
+    }
+
+    // Notify log if there are any arguments that aren't processed.
+    if(unprocessed.length > 0) {
+      landmark.log("[landmark] Unprocessed arguments: " + unprocessed.join(","))
+    }
+
+    // Loop over method invocations and actually invoke them.
+    for(var i=0; i<invocations.length; i++) {
+      landmark[invocations[i].name].apply(landmark, invocations[i].arguments);
+    }
+  }
+  
+  landmark.__test__ = {
+    setCookie: setCookie,
+    getCookie: getCookie,
+    getCookiesEnabled: getCookiesEnabled,
+    processInvocations: processInvocations,
+  };
 
 
   //--------------------------------------------------------------------------
@@ -399,10 +492,25 @@
   var onhashchange = window.onhashchange;
   window.onhashchange = function() {
     if(config.trackHashChange) {
-      landmark.trackPage({}, {includeHash:true});
+      landmark.trackPageView({}, {includeHash:true});
     }
     if(typeof(onhashchange) == "function") onhashchange();
   }
 
+  // Save invocations made before the library was loaded.
+  invocations = window.landmark;
   window.landmark = landmark;
+
+  // Retrieve data fields set on the script tag itself.
+  var script = document.getElementsByTagName("script");
+  script = script[script.length - 1];
+  if(script.hasAttribute('data-api-key')) {
+    landmark.initialize(script.getAttribute('data-api-key'))
+  }
+  if(script.hasAttribute('data-user-id')) {
+    landmark.identify(script.getAttribute('data-user-id'))
+  }
+
+  // Process early invocations.
+  processInvocations(invocations);
 })();
